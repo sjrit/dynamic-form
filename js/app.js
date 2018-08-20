5f72d3b5bdcee25f1b7805d062c8315a924fbd23
Raj Kumar Dubey <raj.sstechnologies@gmail.com>
2018-08-21 02:57:44 +0530

    my repo

--- js/app.js
+++ js/app.js
@@ -35,8 +35,11 @@ xhttp.onreadystatechange = function () {
             else if (response[i].type == "textarea") {
                 textareaFunction(response[i].title);
             }
-            else{
-                radioBoxFunction(response[i].title);
+            else if (response[i].type == "checkbox") {
+                checkBoxFunction(response[i].title, response[i].id);
+            }
+            else if (response[i].type == "radio") {
+                radioBoxFunction(response[i].title, response[i].id);
             }
         }
 
@@ -45,6 +48,17 @@ xhttp.onreadystatechange = function () {
 xhttp.open("GET", "http://localhost:8000/questions", true);
 xhttp.send();
 
+
+function getOptionsData(id) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            return JSON.parse(xhttp.responseText);
+        }
+    };
+    xhttp.open("GET", "http://localhost:8000/options?question=" + id, true);
+    xhttp.send();
+}
 
 
 /* 
@@ -81,8 +95,9 @@ functions for Name text field
 ---------------------------------------------------------------------------
 */
 function nameFunction(title) {
-    var r = document.createElement('span');
+    var r = document.createElement('div');
     var y = document.createElement("INPUT");
+    r.setAttribute("class", "type-1");
     y.setAttribute("type", "text");
     y.setAttribute("placeholder", title);
     var g = document.createElement("IMG");
@@ -105,8 +120,9 @@ functions  for Email text field
 ------------------------------------------------------------------------------
 */
 function emailFunction(title) {
-    var r = document.createElement('span');
+    var r = document.createElement('div');
     var y = document.createElement("INPUT");
+    r.setAttribute("class", "type-1");
     y.setAttribute("type", "text");
     y.setAttribute("placeholder", title);
     var g = document.createElement("IMG");
@@ -129,8 +145,9 @@ functions  for Contact text field
 */
 
 function contactFunction(title) {
-    var r = document.createElement('span');
+    var r = document.createElement('div');
     var y = document.createElement("INPUT");
+    r.setAttribute("class", "type-1");
     y.setAttribute("type", "text");
     y.setAttribute("placeholder", title);
     var g = document.createElement("IMG");
@@ -152,19 +169,36 @@ functions  for radio field
 ------------------------------------------------------------------------------
 */
 
-function radioBoxFunction(title) {
-    // create the necessary elements
-    var label = document.createElement("label");
-    var description = document.createTextNode(pair);
-    var checkbox = document.createElement("input");
+function radioBoxFunction(title, id) {
+    var r = document.createElement('div');
+    var label = document.createElement('label')
 
-    checkbox.type = "checkbox";    // make the element a checkbox
-    checkbox.name = "slct[]";      // give it a name we can check on the server side
-    checkbox.value = pair;         // make its value "pair"
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(xhttp.responseText);
 
-    label.appendChild(checkbox);   // add the box to the element
-    label.appendChild(description);// add the description to the element
-    document.getElementById('myForm').appendChild(label);
+            for (i = 0; i < response.length; i++) {
+                var y = document.createElement("INPUT");
+                y.setAttribute('type', 'radio');
+                y.setAttribute('value', response[i].value);
+                y.setAttribute("Name", "textelement_" + i);
+                r.appendChild(y);
+                label.htmlFor = response[i].value;
+                label.appendChild(document.createTextNode(response[i].label.toString()));
+                r.appendChild(label);
+            }
+            var g = document.createElement("IMG");
+            g.setAttribute("src", "delete.png");
+            increment();
+            g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
+            r.appendChild(g);
+            r.setAttribute("id", "id_" + i);
+            document.getElementById("myForm").appendChild(r);
+        }
+    };
+    xhttp.open("GET", "http://localhost:8000/options?question=" + id, true);
+    xhttp.send();
 }
 
 /*
@@ -175,20 +209,39 @@ functions  for checkbox field
 ------------------------------------------------------------------------------
 */
 
-function checkBoxFunction(title) {
-    var r = document.createElement('span');
-    var y = document.createElement("INPUT");
-    y.setAttribute("type", "checkbox");
-    y.setAttribute("placeholder", title);
-    var g = document.createElement("IMG");
-    g.setAttribute("src", "delete.png");
-    increment();
-    y.setAttribute("Name", "textelement_" + i);
-    r.appendChild(y);
-    g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
-    r.appendChild(g);
-    r.setAttribute("id", "id_" + i);
-    document.getElementById("myForm").appendChild(r);
+function checkBoxFunction(title, id) {
+
+    var r = document.createElement('div');
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(xhttp.responseText);
+
+            for (i = 0; i < response.length; i++) {
+                var y = document.createElement("INPUT");
+                var label = document.createElement('label')
+                y.setAttribute('type', 'checkbox');
+                y.setAttribute('value', response[i].value);
+                y.setAttribute("Name", "textelement_" + i);
+                r.appendChild(y);
+                label.htmlFor = response[i].value;
+                label.appendChild(document.createTextNode(response[i].label.toString()));
+                r.appendChild(label);
+            }
+            var g = document.createElement("IMG");
+            g.setAttribute("src", "delete.png");
+            increment();
+            g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
+            r.appendChild(g);
+            r.setAttribute("id", "id_" + i);
+            document.getElementById("myForm").appendChild(r);
+        }
+    };
+    xhttp.open("GET", "http://localhost:8000/options?question=" + id, true);
+    xhttp.send();
+
+
+
 }
 
 /*
@@ -200,7 +253,7 @@ functions  for textarea field
 */
 
 function textareaFunction(title) {
-    var r = document.createElement('span');
+    var r = document.createElement('div');
 
     var y = document.createElement("TEXTAREA");
     var g = document.createElement("IMG");
