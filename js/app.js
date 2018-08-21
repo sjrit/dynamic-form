d8f1310a019e8632e9e0f132b573b5466296f102
irajmrperfect <i.rajmrperfect@gmail.com>
2018-08-21 16:15:01 +0530

    final

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 /* Your code */
 /* set global variable i */
 
-var i = 0;
+var index = 0;
+var response;
 
 function increment() {
-    i += 1;
+    index++;
     /* function for automatic increament of feild's "Name" attribute*/
 }
 
@@ -19,10 +20,8 @@ function to extract form data
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        var response = JSON.parse(xhttp.responseText);
-        for (var i = 0; i < response.length; i++) {
-            console.log(response[i].type);
-
+        response = JSON.parse(xhttp.responseText);
+        for (let i = 0; i < response.length; i++) {
             if (response[i].type == "text") {
                 nameFunction(response[i].title);
             }
@@ -41,6 +40,9 @@ xhttp.onreadystatechange = function () {
             else if (response[i].type == "radio") {
                 radioBoxFunction(response[i].title, response[i].id);
             }
+            else if (response[i].type == "rating") {
+                ratingFunction(response[i].title);
+            }
         }
 
     }
@@ -49,41 +51,29 @@ xhttp.open("GET", "http://localhost:8000/questions", true);
 xhttp.send();
 
 
-function getOptionsData(id) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            return JSON.parse(xhttp.responseText);
-        }
-    };
-    xhttp.open("GET", "http://localhost:8000/options?question=" + id, true);
-    xhttp.send();
-}
-
-
 /* 
----------------------------------------------
-
-function to remove fom elements dynamically
----------------------------------------------
-
+----------------------------------------------------------------------------
+ 
+functions for Name text field
+ 
+---------------------------------------------------------------------------
 */
-
-function removeElement(parentDiv, childDiv) {
-
-
-    if (childDiv == parentDiv) {
-        alert("The parent div cannot be removed.");
-    }
-    else if (document.getElementById(childDiv)) {
-        var child = document.getElementById(childDiv);
-        var parent = document.getElementById(parentDiv);
-        parent.removeChild(child);
-    }
-    else {
-        alert("Child div has already been removed or does not exist.");
-        return false;
-    }
+function ratingFunction(title) {
+    let r = document.createElement('div');
+    let label = document.createElement('label');
+    let y = document.createElement("INPUT");
+    r.appendChild(label);
+    label.appendChild(document.createTextNode(title));
+    r.setAttribute("class", "type-1");
+    y.setAttribute("type", "range");
+    y.setAttribute("min", 1);
+    y.setAttribute("max", 10);
+    y.setAttribute("value", 5);
+    increment();
+    y.setAttribute("Name", "textelement_" + index);
+    r.appendChild(y);
+    r.setAttribute("id", "id_" + index);
+    document.getElementById("myForm").appendChild(r);
 }
 
 
@@ -95,19 +85,15 @@ functions for Name text field
 ---------------------------------------------------------------------------
 */
 function nameFunction(title) {
-    var r = document.createElement('div');
-    var y = document.createElement("INPUT");
+    let r = document.createElement('div');
+    let y = document.createElement("INPUT");
     r.setAttribute("class", "type-1");
     y.setAttribute("type", "text");
     y.setAttribute("placeholder", title);
-    var g = document.createElement("IMG");
-    g.setAttribute("src", "delete.png");
     increment();
-    y.setAttribute("Name", "textelement_" + i);
+    y.setAttribute("Name", "textelement_" + index);
     r.appendChild(y);
-    g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
-    r.appendChild(g);
-    r.setAttribute("id", "id_" + i);
+    r.setAttribute("id", "id_" + index);
     document.getElementById("myForm").appendChild(r);
 }
 
@@ -120,19 +106,15 @@ functions  for Email text field
 ------------------------------------------------------------------------------
 */
 function emailFunction(title) {
-    var r = document.createElement('div');
-    var y = document.createElement("INPUT");
+    let r = document.createElement('div');
+    let y = document.createElement("INPUT");
     r.setAttribute("class", "type-1");
     y.setAttribute("type", "text");
     y.setAttribute("placeholder", title);
-    var g = document.createElement("IMG");
-    g.setAttribute("src", "delete.png");
     increment();
-    y.setAttribute("Name", "textelement_" + i);
+    y.setAttribute("Name", "textelement_" + index);
     r.appendChild(y);
-    g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
-    r.appendChild(g);
-    r.setAttribute("id", "id_" + i);
+    r.setAttribute("id", "id_" + index);
     document.getElementById("myForm").appendChild(r);
 }
 
@@ -145,19 +127,15 @@ functions  for Contact text field
 */
 
 function contactFunction(title) {
-    var r = document.createElement('div');
-    var y = document.createElement("INPUT");
+    let r = document.createElement('div');
+    let y = document.createElement("INPUT");
     r.setAttribute("class", "type-1");
     y.setAttribute("type", "text");
     y.setAttribute("placeholder", title);
-    var g = document.createElement("IMG");
-    g.setAttribute("src", "delete.png");
     increment();
-    y.setAttribute("Name", "textelement_" + i);
+    y.setAttribute("Name", "textelement_" + index);
     r.appendChild(y);
-    g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
-    r.appendChild(g);
-    r.setAttribute("id", "id_" + i);
+    r.setAttribute("id", "id_" + index);
     document.getElementById("myForm").appendChild(r);
 }
 
@@ -166,34 +144,34 @@ function contactFunction(title) {
 
 functions  for radio field
 
+    
 ------------------------------------------------------------------------------
 */
 
 function radioBoxFunction(title, id) {
-    var r = document.createElement('div');
-    var label = document.createElement('label')
-
+    let r = document.createElement('div');
+    let p = document.createElement('div');
+    let s = document.createElement('p');
     let xhttp = new XMLHttpRequest();
+    increment();
+    let index = this.index;
+    r.setAttribute("id", "id_" + index);
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let response = JSON.parse(xhttp.responseText);
-
-            for (i = 0; i < response.length; i++) {
-                var y = document.createElement("INPUT");
-                y.setAttribute('type', 'radio');
-                y.setAttribute('value', response[i].value);
-                y.setAttribute("Name", "textelement_" + i);
-                r.appendChild(y);
-                label.htmlFor = response[i].value;
-                label.appendChild(document.createTextNode(response[i].label.toString()));
-                r.appendChild(label);
+            for (let i = 0; i < response.length; i++) {
+                let label = document.createElement("label");
+                let radio = document.createElement("input");
+                radio.type = "radio";
+                radio.name = "textelement_" + index;
+                radio.value = response[i].value;
+                label.appendChild(radio);
+                label.appendChild(document.createTextNode(response[i].label));
+                p.appendChild(label);
             }
-            var g = document.createElement("IMG");
-            g.setAttribute("src", "delete.png");
-            increment();
-            g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
-            r.appendChild(g);
-            r.setAttribute("id", "id_" + i);
+            s.appendChild(document.createTextNode(title));
+            r.appendChild(s);
+            r.appendChild(p);
             document.getElementById("myForm").appendChild(r);
         }
     };
@@ -210,30 +188,29 @@ functions  for checkbox field
 */
 
 function checkBoxFunction(title, id) {
-
-    var r = document.createElement('div');
+    let r = document.createElement('div');
+    let p = document.createElement('div');
+    let s = document.createElement('p');
+    increment();
+    let index = this.index;
     let xhttp = new XMLHttpRequest();
+    r.setAttribute("id", "id_" + index);
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let response = JSON.parse(xhttp.responseText);
-
-            for (i = 0; i < response.length; i++) {
-                var y = document.createElement("INPUT");
-                var label = document.createElement('label')
-                y.setAttribute('type', 'checkbox');
-                y.setAttribute('value', response[i].value);
-                y.setAttribute("Name", "textelement_" + i);
-                r.appendChild(y);
-                label.htmlFor = response[i].value;
-                label.appendChild(document.createTextNode(response[i].label.toString()));
-                r.appendChild(label);
+            for (let i = 0; i < response.length; i++) {
+                let label = document.createElement("label");
+                let radio = document.createElement("input");
+                radio.type = "checkbox";
+                radio.name = "textelement_" + index;
+                radio.value = response[i].value;
+                label.appendChild(radio);
+                label.appendChild(document.createTextNode(response[i].label));
+                p.appendChild(label);
             }
-            var g = document.createElement("IMG");
-            g.setAttribute("src", "delete.png");
-            increment();
-            g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
-            r.appendChild(g);
-            r.setAttribute("id", "id_" + i);
+            s.appendChild(document.createTextNode(title));
+            r.appendChild(s);
+            r.appendChild(p);
             document.getElementById("myForm").appendChild(r);
         }
     };
@@ -253,19 +230,19 @@ functions  for textarea field
 */
 
 function textareaFunction(title) {
-    var r = document.createElement('div');
+    let r = document.createElement('div');
 
-    var y = document.createElement("TEXTAREA");
-    var g = document.createElement("IMG");
+    let y = document.createElement("TEXTAREA");
+
     y.setAttribute("cols", "17");
     y.setAttribute("placeholder", title);
-    g.setAttribute("src", "delete.png");
+
     increment();
-    y.setAttribute("Name", "textelement_" + i);
+    y.setAttribute("Name", "textelement_" + index);
     r.appendChild(y);
-    g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
-    r.appendChild(g);
-    r.setAttribute("id", "id_" + i);
+
+
+    r.setAttribute("id", "id_" + index);
     document.getElementById("myForm").appendChild(r);
 
 }
@@ -273,11 +250,45 @@ function textareaFunction(title) {
 /*
 -----------------------------------------------------------------------------
 
-functions  that will be called upon, when user click on the Reset Button
+functions  that will be called upon, when user click on the submit Button
 
 ------------------------------------------------------------------------------
 */
-function resetElements() {
-    document.getElementById('myForm').innerHTML = '';
+function validateForm() {
+    for (let i = 0; i < response.length; i++) {
+        if (response[i].mandatory && !response[i].has_options) {
+            if(document.forms["myForm"]["textelement_" + (i + 1)].value == ''){
+                return
+            }
+        }
+        else if (response[i].mandatory && response[i].has_options) {
+            if (response[i].type == "checkbox") {
+                var chk_arr = document.forms["myForm"]["textelement_" + (i + 1)];
+                var chklength = chk_arr.length;
+                let isChecked = false;
+
+                for (k = 0; k < chklength; k++) {
+                    // chk_arr[k].checked = false;
+                    if (chk_arr[k].checked) {
+                        isChecked = chk_arr[k].checked;
+                    }
+                }
+                if (!isChecked) {
+                    return
+                }
+            }
+            else if (document.forms["myForm"]["textelement_" + (i + 1)].value == '') {
+                return
+
+            }
+
+        }
+
+
+
+
+    }
+    alert("form submitted")
+
 }
 
